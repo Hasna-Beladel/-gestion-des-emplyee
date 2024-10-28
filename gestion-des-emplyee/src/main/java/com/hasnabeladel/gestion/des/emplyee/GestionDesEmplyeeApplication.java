fcf18d59cbd37f7ca66217cd87958593f09a7b2b
BELADEL Hasna <h.beladel3619@uca.ac.ma>
2024-10-28 09:20:32 +0100

    commit1

---
+++ gestion-des-emplyee/src/main/java/com/hasnabeladel/gestion/des/emplyee/GestionDesEmplyeeApplication.java
@@ -0,0 +1,13 @@
+package com.hasnabeladel.gestion.des.emplyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesEmplyeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesEmplyeeApplication.class, args);
+	}
+
+}
---
+++ gestion-des-emplyee/src/test/java/com/hasnabeladel/gestion/des/emplyee/GestionDesEmplyeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasnabeladel.gestion.des.emplyee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesEmplyeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
